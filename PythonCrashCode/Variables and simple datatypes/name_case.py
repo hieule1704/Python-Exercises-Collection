4e3c677df27a8f69d06b6975c732bbc765c07f84
Hieu Dep Trai <lehieu17042004@gmail.com>
2024-03-30 18:38:53 +0700

    Submid some Python excercises

---
+++ PythonCrashCode/Variables and simple datatypes/name_case.py
@@ -0,0 +1,6 @@
+#2-3. Personal Message: Use a variable to represent a person’s name, and print a message to that person. Your message should be simple, suchjson as, “Hello Eric, would you like to learn some Python today?”
+name = input("Enter person's name you want to say Hello: ")
+message = f"Hello {name.title()}, would you like to learn some Python today?"
+print(message)
+
+#2-4. Name Cases: Use a variable to represent a person’s name, and then print that person’s name in lowercase, uppercase, and title case.
