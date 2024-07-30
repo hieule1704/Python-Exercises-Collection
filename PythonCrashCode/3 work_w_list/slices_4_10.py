f00a82e8812afa6a2256fbce1bf2a57a5a7a7258
Hieu Data <lehieu17042004@gmail.com>
2024-07-30 07:53:18 +0700

    Daily coding

---
+++ PythonCrashCode/3 work_w_list/slices_4_10.py
@@ -0,0 +1,7 @@
+exs = ["Ngọc","Điều","Anh","Ý","Vẹn","Ngân","Loan"]
+print("The first three ex:")
+print(exs[:3])
+print("The middle ex of the list: ")
+print(exs[len(exs)//2])
+print("The last three ex:")
+print(exs[-3:])
