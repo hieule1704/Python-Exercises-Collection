52a2bcfc3065d6887f2d5ea683b0811c605700dd
Hieu Data <lehieu17042004@gmail.com>
2024-08-12 19:46:59 +0700

    Daily coding

---
+++ Bài tập Chuyên đề Python/Chương 5/Cau2.py
@@ -0,0 +1,26 @@
+def formatString(string):
+    # Xóa khoảng trắng ở đầu và cuối chuỗi
+    string = string.strip()
+
+    # Chuyển chuỗi thành danh sách các ký tự
+    strings = list(string)
+    index = 0
+
+    # Xóa các khoảng trắng thừa giữa các từ
+    while index < len(strings) - 1:
+        if strings[index] == ' ' and strings[index + 1] == ' ':
+            strings.pop(index)
+        else:
+            index += 1
+
+    # Chuyển danh sách ký tự trở lại thành chuỗi
+    return ''.join(strings)
+
+
+def main():
+    string = input("Enter your string here: ")
+    print("Formatted string:", formatString(string))
+
+
+if __name__ == '__main__':
+    main()
